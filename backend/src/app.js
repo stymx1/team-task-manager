579019242990500b14d5aa0a548eb3750ff4b508
stymx1 <satyamchauhan137@gmail.com>
2026-05-03 19:48:31 +0530

    fix: port + root route

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/projects", require("./routes/projectRoutes"));
