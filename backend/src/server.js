b4a750715c72123528ac9fec1e337e9f5f8fecf9
stymx1 <satyamchauhan137@gmail.com>
2026-05-03 19:18:39 +0530

    fix port issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const app = require("./app");
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on ${process.env.PORT}`)
-);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
